3beb060bddf955a30166847e254555853ad678ba
Arash Abs <araash.abs@gmail.com>
2021-04-19 16:58:51 +0430

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var channel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GrpcServiceIntro.Greeter.GreeterClient(channel);
+
+            //var response = await client.SayHelloAsync(new GrpcServiceIntro.HelloRequest
+            //{
+            //    Name = ".Net Conf"
+            //});
+
+            //Console.WriteLine($"From Server : {response.Message}");
+
+            var call = client.SayHelloStream(new GrpcServiceIntro.HelloRequest
+            {
+                Name = $".Net Conf"
+            });
+
+            await foreach (var item in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"From server streaming { item.Message }");
+            }
+        }
+    }
+}
